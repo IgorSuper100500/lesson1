d93a50c49597c46e9b98b2886ecb03307b20b106
IgorSuper100500 <166173917+IgorSuper100500@users.noreply.github.com>
2024-12-03 07:58:02 +0300

    Add files via upload

---
+++ module_5_2_1.py
@@ -0,0 +1,48 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if new_floor <= self.number_of_floors and new_floor!=0:
+            print('-----------------------------------------------')
+            print(f'В {self.name} {self.number_of_floors} этажей. Мы едем на {self.new_floor} этаж')
+            for i in range(new_floor):
+                print(i + 1)
+        else:
+            print('-----------------------------------------------')
+            print(f'В ЖК {self.name} такого этажа не существует')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h3 = House('Красный октябрь', 25)
+
+# Лифт
+h1.go_to(5)
+h2.go_to(10)
+h3.go_to(7)
+
+# __str__
+print('++++++++++++++++++++++++++++++++++++++++++++++++')
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
+print('++++++++++++++++++++++++++++++++++++++++++++++++')
+
+
+print('------------------------------------------')
+print(h1.name, h1.number_of_floors)
+print(h2.name, h2.number_of_floors)
+print(h3.name, h3.number_of_floors)
